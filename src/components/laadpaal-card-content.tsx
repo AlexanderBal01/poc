1805fa51aadf385c5acbc49f68054788f99dd414
Alexander Bal <alexander.bal@student.hogent.be>
2024-06-06 19:46:16 +0200

    changes

---
+++ src/components/laadpaal-card-content.tsx
@@ -0,0 +1,64 @@
+import React, { useEffect } from 'react';
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from './ui/card';
+import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from './ui/carousel';
+import { log } from 'console';
+
+export const LaadpaalCardCarouselContent = (props) => {
+	const { laadpaalInfo } = props;
+	console.log(laadpaalInfo);
+
+	return (
+		<Carousel>
+			<CarouselContent>
+				<CarouselItem key={1}>
+					<div className=' flex flex-col rounded-md border w-10/12'>
+						<div className='flex justify-center '>
+							<h3 className='text-2xl text-center'>Informatie</h3>
+						</div>
+						<p className='mx-2'>
+							Maximale Stroom:{' '}
+							<span className='float-right'>
+								<b></b>
+							</span>
+						</p>
+					</div>
+				</CarouselItem>
+				<CarouselItem key={2}>
+					<Card>
+						<CardHeader>
+							<CardTitle>Informatie</CardTitle>
+						</CardHeader>
+						<CardContent>
+							<CardDescription>Laadpaal 1</CardDescription>
+						</CardContent>
+					</Card>
+				</CarouselItem>
+			</CarouselContent>
+		</Carousel>
+	);
+};
+
+export const LaadpaalCardContent = (props) => {
+	const { laadpaalInfo } = props;
+
+	return (
+		<>
+			<Card>
+				<CardHeader>
+					<CardTitle>Informatie</CardTitle>
+				</CardHeader>
+				<CardContent>
+					<CardDescription>Laadpaal 1</CardDescription>
+				</CardContent>
+			</Card>
+			<Card>
+				<CardHeader>
+					<CardTitle>Informatie</CardTitle>
+				</CardHeader>
+				<CardContent>
+					<CardDescription>Laadpaal 1</CardDescription>
+				</CardContent>
+			</Card>
+		</>
+	);
+};
--- src/components/laadpaal-card.tsx
+++ src/components/laadpaal-card.tsx
@@ -1,14 +1,46 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
+import { LaadpaalCardCarouselContent, LaadpaalCardContent } from './laadpaal-card-content';
 
 const LaadpaalCard = () => {
+	const [isMobile, setIsMobile] = useState(false);
+	const [data, setData] = useState({});
+
+	useEffect(() => {
+		const handleResize = () => {
+			setIsMobile(window.innerWidth <= 768);
+		};
+
+		// Initialize the state
+		handleResize();
+
+		// Add event listener
+		window.addEventListener('resize', handleResize);
+
+		// Clean up the event listener
+		return () => window.removeEventListener('resize', handleResize);
+	}, []);
+
+	useEffect(() => {
+		const interval = setInterval(async () => {
+			const res = await fetch(`http://192.168.1.61:1880/charging-station/info?token=${localStorage.getItem('token')}`, {
+				method: 'GET',
+				headers: {
+					'Content-Type': 'application/json',
+				},
+			});
+
+			const response = await res.json();
+			setData([response[0]]);
+		}, 1000);
+	});
+
 	return (
-		<Card className='my-5 mx-10'>
+		<Card className='my-5 mx-10 md'>
 			<CardHeader className='items-center'>
 				<CardTitle className='items-center justify-center'>Laadpaal</CardTitle>
-				<CardDescription>informatie</CardDescription>
 			</CardHeader>
-			<CardContent></CardContent>
+			<CardContent className='flex space-x-5'>{isMobile ? <LaadpaalCardCarouselContent laadpaalInfo={data} /> : <LaadpaalCardContent laadpaalInfo={data} />}</CardContent>
 		</Card>
 	);
 };
